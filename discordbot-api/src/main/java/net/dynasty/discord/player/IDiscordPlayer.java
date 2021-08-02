5c7d03cb579b41734dcd46a229fca129a967ea97
tylix <maximilian.wiegmann@icloud.com>
2021-08-02 06:37:16 +0200

    Added Discord Player

---
+++ discordbot-api/src/main/java/net/dynasty/discord/player/IDiscordPlayer.java
@@ -0,0 +1,29 @@
+package net.dynasty.discord.player;
+
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
+import net.dynasty.discord.permission.IPermissionObject;
+import net.verany.api.interfaces.IDefault;
+
+public interface IDiscordPlayer extends IDefault<Long> {
+
+    IPermissionObject getPermissionObject();
+
+    String getName();
+
+    String getNickname();
+
+    User getUser();
+
+    Member getMember();
+
+    void sendMessage(String... messages);
+
+    void sendMessage(MessageEmbed... messages);
+
+    void setNickname(String nickname);
+
+    void onJoin();
+
+}
