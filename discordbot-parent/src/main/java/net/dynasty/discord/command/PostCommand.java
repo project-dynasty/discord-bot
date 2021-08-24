f7bd8367d12977a3b60173af4ccdedb816adca46
tylix <maximilian.wiegmann@icloud.com>
2021-08-25 01:35:58 +0200

    Implemented Post Command to Slash Commands

--- discordbot-parent/src/main/java/net/dynasty/discord/command/PostCommand.java
+++ discordbot-parent/src/main/java/net/dynasty/discord/command/PostCommand.java
@@ -2,9 +2,15 @@ package net.dynasty.discord.command;
 
 import lombok.SneakyThrows;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.components.Button;
 import net.dynasty.discord.DiscordBot;
 import net.dynasty.discord.permission.PermissionGroupLoader;
 import net.dynasty.discord.player.IDiscordPlayer;
@@ -16,6 +22,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
@@ -30,13 +37,66 @@ public class PostCommand extends AbstractCommand {
         super(name);
         setPermissionGroups(PermissionGroupLoader.MANAGEMENT, PermissionGroupLoader.MODERATOR, PermissionGroupLoader.CONCEPTION);
         setChannel(871677780117565440L);
-        setDescription("Auch moin");
+        setDescription("Here you can create a message sent by the Bot");
+        addOption(new OptionData(OptionType.CHANNEL, "channel", "The channel where the message will be sent", true),
+                new OptionData(OptionType.STRING, "message", "The message", true),
+                new OptionData(OptionType.BOOLEAN, "embed", "Is the message embed", false),
+                new OptionData(OptionType.STRING, "color", "The color of the embed message", false).
+                        addChoice("green", "green").
+                        addChoice("cyan", "cyan").
+                        addChoice("red", "red").
+                        addChoice("yellow", "yellow").
+                        addChoice("orange", "orange").
+                        addChoice("blue", "blue")
+                ,
+                new OptionData(OptionType.STRING, "title", "The title of the message", false),
+                new OptionData(OptionType.BOOLEAN, "footer", "Sending Footer Credits", false));
+    }
+
+    @Override
+    public void onButtonClick(IDiscordPlayer user, ButtonClickEvent clickEvent, String name) {
+        switch (name) {
+            case "post" -> {
+                String postId = clickEvent.getComponentId().split("_")[2];
+                queuedPosts.stream().filter(post -> post.getId().equals(postId)).findFirst().ifPresent(post -> {
+                    post.send();
+                    clickEvent.replyEmbeds(new EmbedBuilder().setColor(Color.green).setDescription("You successfully posted in " + DiscordBot.INSTANCE.getGuild().getTextChannelById(post.getTextChannel().getIdLong()).getAsMention()).build()).setEphemeral(true).queue();
+                });
+            }
+        }
     }
 
     @SneakyThrows
     @Override
     public void onExecute(IDiscordPlayer user, SlashCommandEvent event) {
-        System.out.println("run command");
+        OptionMapping channel = event.getOption("channel");
+        OptionMapping message = event.getOption("message");
+        boolean emebed = event.getOption("embed") != null && event.getOption("embed").getAsBoolean();
+        Color color = getColor(event.getOption("color") != null ? event.getOption("color").getAsString() : "cyan");
+        String title = event.getOption("title") != null ? event.getOption("title").getAsString() : null;
+        boolean footer = event.getOption("footer") == null || event.getOption("footer").getAsBoolean();
+
+        MessageChannel textChannel = channel.getAsMessageChannel();
+        String messageString = message.getAsString();
+
+        Post post = new Post(user, messageString, textChannel);
+
+        if (emebed) {
+            EmbedBuilder builder = new EmbedBuilder().setDescription(messageString).setTitle(title).setColor(color);
+            if (footer)
+                builder.setFooter("Dynasty.net " + new SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis()), DiscordBot.INSTANCE.getJda().getSelfUser().getAvatarUrl());
+            post.setEmbedBuilder(builder);
+        }
+
+        queuedPosts.add(post);
+        event.replyEmbeds(new EmbedBuilder().setTitle("Please confirm your action").setDescription("**Message**:\n" + messageString).setColor(Color.cyan).build()).setEphemeral(true).addActionRow(Button.success(buttonName("post_" + post.getId()), "Post"), Button.secondary(buttonName("cancel"), "Cancel")).queue();
+
+        /*if (emebed) {
+            textChannel.sendMessageEmbeds(new EmbedBuilder().setDescription(messageString).setColor(color).setFooter("Dynasty.net " + new SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis()), DiscordBot.INSTANCE.getJda().getSelfUser().getAvatarUrl()).build()).queue();
+        } else {
+            textChannel.sendMessage(messageString).queue();
+        }*/
+
         /*if (args.length == 1) {
             if (args[0].equalsIgnoreCase("queue")) {
                 StringBuilder posts = new StringBuilder();
@@ -91,5 +151,16 @@ public class PostCommand extends AbstractCommand {
                 return;
             }
         }*/
+    }
+
+    private Color getColor(String name) {
+        Color color;
+        try {
+            Field field = Color.class.getField(name);
+            color = (Color) field.get(null);
+        } catch (Exception e) {
+            color = null; // Not defined
+        }
+        return color;
     }
 }
--- discordbot-parent/src/main/java/net/dynasty/discord/post/Post.java
+++ discordbot-parent/src/main/java/net/dynasty/discord/post/Post.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dynasty.discord.DiscordBot;
 import net.dynasty.discord.player.IDiscordPlayer;
@@ -18,16 +19,16 @@ public class Post {
     private final long timestamp = System.currentTimeMillis();
     private final IDiscordPlayer sender;
     private final String message;
-    private final TextChannel textChannel;
+    private final MessageChannel textChannel;
     private EmbedBuilder embedBuilder = null;
 
     public void send() {
         send(textChannel);
     }
 
-    public void send(TextChannel textChannel) {
-        if(embedBuilder != null) {
-
+    public void send(MessageChannel textChannel) {
+        if (embedBuilder != null) {
+            textChannel.sendMessageEmbeds(embedBuilder.build()).queue();
             return;
         }
         textChannel.sendMessage(message).queue();
